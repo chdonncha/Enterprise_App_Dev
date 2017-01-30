ef2d5870f7f96c3ed5228638d7778e5494be113a
chdonncha <chdonncha@gmail.com>
2017-01-30 22:08:34 +0000

    created user for postgres, which fixed some errors

--- Lab1/index.js
+++ Lab1/index.js
@@ -1,16 +1,7 @@
-var express = require('express')
-var app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
-
-var Massive=require("massive");
-var db = Massive.connectSync({db : 'massive-test'});
+var massive=require("massive");
+var db = massive.connectSync({
+	connectionString: 'postgres://donncha:pass123@localhost/pgguide'
+});
 
 var newUser = {
   email : "test@test.com",

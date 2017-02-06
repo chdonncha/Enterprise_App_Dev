c4045d92e8a5af301354c41e61124075fe86e25f
chdonncha <chdonncha@gmail.com>
2017-02-06 09:57:24 +0000

    complete search id through url on exercise 6, still more to completegit add --all

--- Lab1/index.js
+++ Lab1/index.js
@@ -1,13 +1,23 @@
 var express = require('express')
-var app = express()
+var app = express();
 
 var Massive=require("massive");
 var db = Massive.connectSync({
 	connectionString: 'postgres://postgres:pass123@localhost/pgguide'
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/users', function (req, res) {
+  db.users.find({}, function (err, data) {
+    console.log(data)
+    res.send(data)
+  })
+})
+
+app.get('/users/:id', function (req, res) {
+  db.users.find({id: req.params.id}, function (err, data) {
+      console.log(data)
+    res.send(data)
+  })
 })
 
 app.listen(3000, function () {

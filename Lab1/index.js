b58a62962643146b13ac9e9cc2f4e1886aa9eb61
chdonncha <chdonncha@gmail.com>
2017-02-06 09:31:01 +0000

    added massive requirement to exercise 6

--- Lab1/index.js
+++ Lab1/index.js
@@ -1,6 +1,11 @@
 var express = require('express')
 var app = express()
 
+var Massive=require("massive");
+var db = Massive.connectSync({
+	connectionString: 'postgres://postgres:pass123@localhost/pgguide'
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })

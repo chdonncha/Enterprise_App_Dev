bc01f68b0729bf379a51a49e5020c624cdf17544
chdonncha <chdonncha@gmail.com>
2017-02-06 12:12:56 +0000

    updates, attempting exercise 5, not finished

--- lab1/exercise_5.js
+++ lab1/exercise_5.js
@@ -3,6 +3,35 @@ var db = Massive.connectSync({
 	connectionString: 'postgres://postgres:pass123@localhost/pgguide'
 });
 
+
+var newUser = {
+  email : "test@test.com",
+  first : "Joe",
+  last : "Test"
+};
+
+db.users.save(newUser, function(err,result){
+  console.log(result);  
+});
+
+db.users.find({email : "test@test.com"}, function(err,res){
+  console.log(res);
+});
+
+/*
+db.users.saveSync({id: 1, email : "joe@test.com"});
+var joe = db.users.findSync(1);
+console.log(joe);
+
+var joe = db.users.saveSync({id: 1, email : "joe@test.com"});
+console.log(joe);
+
+db.users.destroy({id: 1}, function(err,res){
+  console.log(res);
+});
+*/
+
+/*
 console.log(db.users);
 db.users.find({}, function(err,res){
   console.log(res);
@@ -18,7 +47,8 @@ db.purchases.find({}, function(err,res){
   console.log(res);
 });
 
-console.log(db.purchase-items);
+console.log(db.purchase_items);
 db.purchase_items.find({}, function(err,res){
   console.log(res);
 });
+*/

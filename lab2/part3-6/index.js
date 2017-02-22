356f7576ed5882ab90a8e877f8bb71aa8bf71bae
chdonncha <chdonncha@gmail.com>
2017-02-22 14:08:24 +0000

    updates (still not working)

--- lab2/part3-6/index.js
+++ lab2/part3-6/index.js
@@ -7,45 +7,26 @@ var db = Massive.connectSync({
 	connectionString: 'postgres://postgres:pass123@localhost/lab2'
 });
 
-// var sequelize = new Sequelize('lab2', 'postgres', 'pass123');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('postgres://postgres:pass123@localhost/lab2');
 
-// var User = sequelize.define('user', {
-//   username: Sequelize.STRING,
-//   birthday: Sequelize.DATE
-// });
+// sequelize.courtroom.saveSync({number: 1});
+// var joe = sequelize.courtroom.findSync(1);
+// console.log("\n \n save sync user: \n" );
+// console.log(joe);
 
-// sequelize.sync().then(function() {
-//   return User.create({
-//     username: 'janedoe',
-//     birthday: new Date(1980, 6, 20)
-//   });
-// }).then(function(jane) {
-//   console.log(jane.get({
-//     plain: true
-//   }));
-// });
+var models = require('./models/index');
 
-// //establish connection with database
-// var db = Massive.connectSync({
-// 	connectionString: 'postgres://postgres:pass123@localhost/lab2'
-// });
+app.post('/users', function(req, res) {
+  models.User.create({
+    email: req.body.email
+  }).then(function(user) {
+    res.json(user);
+  });
+});
 
-// var newJudge = {
-//   name : "john",
-// };
 
-// //save new user to database
-// var saved = db.judge.saveSync(newJudge);
-// console.log("\n \n create new judge: \n");
-// console.log(saved);
-
-// //find all details on the newly created user
-// db.judges.find({name : "john"}, function(err,res){
-//   console.log("\n \n find newly created judge: \n");
-//   console.log(res);
-// });
-
-// var models = require('./models/index');
+// 
 
 // app.post('/judges', function(req, res) {
 //   models.judges.create({
@@ -55,13 +36,13 @@ var db = Massive.connectSync({
 //   });
 // });
 
-app.post('/courtrooms', function(req, res) {
-  app.Courtrooms.create({
-    number: req.body.number
-  }).then(function(courtroom) {
-    res.json(courtroom);
-  });
-});
+// app.post('/courtrooms', function(req, res) {
+//   app.Courtrooms.create({
+//     number: req.body.number
+//   }).then(function(courtroom) {
+//     res.json(courtroom);
+//   });
+// });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')

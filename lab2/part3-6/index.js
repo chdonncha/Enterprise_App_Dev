e1816e056e408232fce6ec3ae67cb89e98e760bb
donncha <chdonncha@gmail.com>
2017-02-23 19:48:45 +0000

    updates

--- lab2/part3-6/index.js
+++ lab2/part3-6/index.js
@@ -5,13 +5,13 @@ var app = express();
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('postgres://postgres:pass123@localhost/lab2');
 
-const db = require("./models");
+const models = require("./models");
 
-db.sequelize.sync()
-	.then(() => db.Courtroom.destroy({
+models.sequelize.sync()
+	.then(() => models.Courtroom.destroy({
 		where: {}
 	}))
-	.then(() => db.Courtroom.bulkCreate([
+	.then(() => models.Courtroom.bulkCreate([
 		{number: 1},
 		{number: 2},
 		{number: 3},
@@ -19,18 +19,48 @@ db.sequelize.sync()
 	]))
 
 app.get('/courtroom', function(req, res) {
-  db.Courtroom.findAll({}).then(function(courtroom) {
+  models.Courtroom.findAll({}).then(function(courtroom) {
     res.json(courtroom);
   });
 });
 
 app.post('/courtroom_post', function(req, res) {
-  db.Courtroom.findAll({
+  models.Courtroom.findAll({
     number: 6,
   }).then(function(courtroom) {
     res.json(courtroom);
   });
 });
+
+// get single todo
+app.get('/courtroom/:id', function(req, res) {
+  models.Courtroom.find({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(courtroom) {
+    res.json(courtroom);
+  });
+});
+
+
+// update single todo
+app.put('/courtroom/:id', function(req, res) {
+  models.Courtroom.find({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(courtroom) {
+    if(courtroom){
+      courtroom.updateAttributes({
+        number: req.body.number,
+      }).then(function(courtroom) {
+        res.send(courtroom);
+      });
+    }
+  });
+});
+
 
 
 app.listen(3000, function () {

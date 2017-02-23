d201ba476c89f0d49f529a4f83b4782403de27b7
donncha <chdonncha@gmail.com>
2017-02-23 18:25:16 +0000

    fixed errors

--- lab2/part3-6/index.js
+++ lab2/part3-6/index.js
@@ -2,47 +2,22 @@
 var express = require('express')
 var app = express();
 
-var Massive=require("massive");
-var db = Massive.connectSync({
-	connectionString: 'postgres://postgres:pass123@localhost/lab2'
-});
-
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('postgres://postgres:pass123@localhost/lab2');
 
-// sequelize.courtroom.saveSync({number: 1});
-// var joe = sequelize.courtroom.findSync(1);
-// console.log("\n \n save sync user: \n" );
-// console.log(joe);
+const db = require("./models");
 
-var models = require('./models/index');
+db.sequelize.sync()
+	.then(() => db.Courtroom.destroy({
+		where: {}
+	}))
+	.then(() => db.Courtroom.bulkCreate([
+		{number: 1},
+		{number: 2},
+		{number: 3},
+		{number: 4}
+	]))
 
-app.post('/users', function(req, res) {
-  models.User.create({
-    email: req.body.email
-  }).then(function(user) {
-    res.json(user);
-  });
-});
-
-
-// 
-
-// app.post('/judges', function(req, res) {
-//   models.judges.create({
-//     name: req.body.name
-//   }).then(function(judges) {
-//     res.json(judges);
-//   });
-// });
-
-// app.post('/courtrooms', function(req, res) {
-//   app.Courtrooms.create({
-//     number: req.body.number
-//   }).then(function(courtroom) {
-//     res.json(courtroom);
-//   });
-// });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
